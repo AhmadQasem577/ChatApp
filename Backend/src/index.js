25cf52c9dbe20b23ff49fe080d967aa9864a18f8
Ahmad Qasim <aq5000007@gmail.com>
2025-03-27 05:13:15 +0300

    created the user schema, and started with controllers

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,10 +1,28 @@
 import express from 'express';
 import authRoutes from './routes/auth.route.js';
-const app = express();
-const port = 5022;
+import dotenv from 'dotenv';
+import connectDB from './lib/DB.js';
 
+// Load environment variables from .env file
+dotenv.config();
+// Create an express application
+const app = express();
+
+// Define the port number
+const PORT = process.env.PORT ;
+
+// Add middleware to parse JSON data
+// This will allow the application to handle JSON data in the request body
+app.use(express.json());
+
+// Add middleware to parse URL encoded data
+// This will allow the application to handle URL encoded data in the request body
 app.use("/api/auth", authRoutes);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Start the server
+// This will start the server on the specified port number
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  
+  connectDB();
 });
